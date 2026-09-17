a89f4de64b57efaa2c6fc9470bd88219c7187183
lumenth56 <sergiopc27.05.05@gmail.com>
2026-09-17 13:57:59 -0600

    correcion 4

--- palabras.sh
+++ palabras.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-if [ -z $1 ] || [ ! -d $1 ]; then
-	echo "No pusiste ningun argumento o no existe la carpeta"
-	exit 1
+if [ -z $1 ]|| [ ! -d $1 ];then
+        echo "No existe la carpeta o no hay argumento"
+        exit 1
 fi
-
-
+palabra="home"
 carpetas=${1:-/}
-resultados=$(grep -rl -D skip "home" "$carpetas" 2>/dev/null)
+resultados=$(grep -rl -D skip "$palabra" "$carpetas" 2>/dev/null)
 
 if [ -z "$resultados" ]; then
-    echo "No se hallo la palabra $palabra"
+    echo "No se hallo la palabra home"
     exit 0
 fi
 echo "Archivos con home":
