1ab2b530a1692a92d346963358362718ec3d46cf
Constanza Castro <castroconstanza1007@gmail.com>
2026-09-17 12:50:49 -0600

    validacion y salida

--- palabras.sh
+++ palabras.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+if [ -z $# ] || [ ! -d $# ]; then
+	echo "No pusiste ningun argumento o no existe la carpeta"
+	exit 1
+fi
+
+
 carpetas=${1:-/}
 resultados=$(grep -rl -D skip "home" "$carpetas" 2>/dev/null)
+
+if [ -z "$resultados" ]; then
+    echo "No se hallo la palabra $palabra"
+    exit 0
+fi
+echo "Archivos con "home":
+echo "$resultados" | while read -r archivo; do
+    realpath "$archivo"
+done
