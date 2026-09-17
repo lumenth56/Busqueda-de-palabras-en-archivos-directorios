589b5eb6aa406c849ea70dc5bb3a57950ff1e2f5
MacBookPro12 <macbookpro12@MacBook-Pro.local>
2026-09-17 12:16:01 -0600

    Validacion

--- palabras.sh
+++ palabras.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+if [ -z $# ] || [ ! -d $# ]; then
+	echo "No pusiste ningun argumento o no existe la carpeta"
+	exit 1
+fi
+
+
 carpetas=${1:-/}
 resultados=$(grep -rl -D skip "home" "$carpetas" 2>/dev/null)
+
